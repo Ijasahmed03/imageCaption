43f2a6ca49e6de93320ab812d1dbf15f4551e86b
drakeingolf <ijahmed13@gmail.com>
2025-12-16 22:34:05 +0530

    had to delete

--- src/test_dataset.py
+++
@@ -1,8 +0,0 @@
-from dataset import get_dataloader
-
-loader, dataset = get_dataloader("data/sample")
-
-for feats, caps in loader:
-    print("features shape:", feats.shape)
-    print("captions shape:", caps.shape)
-    break
